e6dcac9ad80d9cbcd408d5ab00f1f7de6dbac0e5
Santhosh kumar <34086991+newitsan@users.noreply.github.com>
2018-07-17 18:03:03 +0530

    Add files via upload

---
+++ arith.java
@@ -0,0 +1,50 @@
+	class sample
+	
+		{
+		
+			int x,y;
+			
+			public int display (int a,int b)
+			
+			{
+				
+				x=a;y=b;
+					
+				int tot=x+y;
+				
+				return tot;
+			
+			}
+	
+			
+			public String display1 (String s1,String s2)
+			
+			{
+				
+				String c=s1+s2;
+				
+				return c;
+			
+			}
+	
+		}
+	
+	class arith
+
+		{
+			
+		public static void main(String args[])
+			
+			{
+				
+				sample obj=new sample();
+//				
+				int tot= obj.display (10,10);
+//				
+				String c= obj.display1 ("android","google");
+						System.out.println(obj.display(10,10));
+							System.out.println(obj.display1("android","google"));	
+						}
+	
+		}
+
