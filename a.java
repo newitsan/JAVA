81bdf4b1865665239f44a83014a8f6043a221b02
Santhosh kumar <34086991+newitsan@users.noreply.github.com>
2018-07-17 18:10:31 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,46 @@
+	class sample
+
+		{
+	
+			int a,b;	
+	
+			public int display(int x,int y)
+	
+	
+			{
+		
+				a=x;b=y;
+		
+				int sum=a+b;
+		
+				return sum;
+	
+			}
+	
+			public String display1(String s1,String s2)
+	
+			{
+		
+				String s=s1+s2;
+		
+				return s;
+	
+			}
+		
+}
+	class a
+		
+{
+	
+			public static void main(String args[])
+	
+			{
+		
+			sample obj=new sample();
+		
+			System.out.println("The sum is "+(obj.display(20,30)));
+					System.out.println("The string is "+(obj.display1("hi","hello")));
+				}
+		}
+		
+		
